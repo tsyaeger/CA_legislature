29fd60c72b99fc344e3b249cb84d3de2a53a97eb
tsyaeger <tsyaeger@gmail.com>
2018-06-08 13:35:45 -0700

    working leg scraper

--- scraper.rb
+++ scraper.rb
@@ -4,10 +4,10 @@ require 'nokogiri'
 
 class Scraper
 
-	senate_path = 'http://senate.ca.gov/leadership'
-	assembly_path = 'http://assembly.ca.gov/assemblymembers'
-	leg_path = 'http://leginfo.legislature.ca.gov/faces/billSearchClient.xhtml?session_year=20172018&house=Both&author=All&lawCode=All'
-	bill_path = "http://leginfo.legislature.ca.gov#{url}"
+	# senate_path = 'http://senate.ca.gov/leadership'
+	# assembly_path = 'http://assembly.ca.gov/assemblymembers'
+	# leg_path = 'http://leginfo.legislature.ca.gov/faces/billSearchClient.xhtml?session_year=20172018&house=Both&author=All&lawCode=All'
+	# bill_path = "http://leginfo.legislature.ca.gov#{url}"
 
 
 
@@ -16,9 +16,19 @@ class Scraper
 	end
 
 
+	def self.scrape_leg
+		leg_path = 'http://leginfo.legislature.ca.gov/faces/billSearchClient.xhtml?session_year=20172018&house=Both&author=All&lawCode=All'
 
+		page = scrape_page(leg_path)
 
+		puts page
 
+	end
 
 
 end
+
+
+
+test = Scraper
+test.scrape_leg
