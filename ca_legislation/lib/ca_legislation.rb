57e6a484527d9342cad05f529000b95d00cac7bd
tsyaeger <tsyaeger@gmail.com>
2018-06-11 17:24:49 -0700

    existing code added to env

--- ca_legislation/lib/ca_legislation.rb
+++ ca_legislation/lib/ca_legislation.rb
@@ -1,4 +1,23 @@
-require "ca_legislation/version"
+
+
+require 'bundler'
+require 'nokogiri'
+require 'colorize'
+require 'rubygems'
+require 'open-uri'
+
+# require "ca_legislation/version"
+
+
+
+require_relative "ca_legislation/version"
+require_relative "ca_legislation/cli"
+require_relative "ca_legislation/bill"
+require_relative "ca_legislation/rep"
+require_relative "ca_legislation/party"
+require_relative "ca_legislation/script"
+require_relative "ca_legislation/scraper"
+
 
 module CaLegislation. # WHERE DID THIS COME FROM
   # Your code goes here...
